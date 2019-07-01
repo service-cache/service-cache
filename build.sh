ab09ce6a5a58a3b652c582be6c5bc11b6d6aee74
javafuns <javafuns@qq.com>
2019-07-01 21:27:16 +0800

    variable in sed is not replaced with value

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ IMAGE_NAME="javafuns/servicecache-operator:v0.0.1"
 
 operator-sdk build ${IMAGE_NAME}
 
-sed -i 's|REPLACE_IMAGE|${IMAGE_NAME}|g' deploy/operator.yaml
+sed -i "s|REPLACE_IMAGE|${IMAGE_NAME}|g" deploy/operator.yaml
 
 docker push ${IMAGE_NAME}
 
-sed -i 's|${IMAGE_NAME}|REPLACE_IMAGE|g' deploy/operator.yaml
+sed -i "s|${IMAGE_NAME}|REPLACE_IMAGE|g" deploy/operator.yaml
